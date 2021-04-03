7d4c43c723d6560f4c6893cfb6997502237168c8
Hamid Reza Oujand <hmdrzaa11@gmail.com>
2021-04-03 23:58:00 +0430

    implement restrictTo to create product route

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,9 +1,14 @@
 let router = require("express").Router();
 let productController = require("../controllers/productController");
 let usersController = require("../controllers/userController");
+let restrictTo = require("../utils/restrictTo");
 
 router
   .route("/")
-  .post(usersController.isAuthenticated, productController.createProduct);
+  .post(
+    usersController.isAuthenticated,
+    restrictTo("admin"),
+    productController.createProduct
+  );
 
 module.exports = router;
