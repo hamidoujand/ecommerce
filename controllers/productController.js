793c77e535af8f1300d0c6a41ac12ee1bff535a7
Hamid Reza Oujand <hmdrzaa11@gmail.com>
2021-04-04 01:20:13 +0430

    add and config multer middle

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,20 @@
 let Product = require("../models/Product");
 let catchAsync = require("../utils/catchAsync");
+let multer = require("multer");
+const AppError = require("../utils/AppError");
+
+let multerStorage = multer.memoryStorage();
+
+let upload = multer({
+  storage: multerStorage,
+  fileFilter(req, file, cb) {
+    if (file.mimetype.startsWith("image")) {
+      return cb(null, true);
+    } else {
+      return cb(new AppError("please upload an image", 400));
+    }
+  },
+});
 
 exports.createProduct = catchAsync(async (req, res, next) => {
   let product = await Product.create(req.body);
