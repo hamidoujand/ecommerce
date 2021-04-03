9d488db14144e22523f0bcc97671992713667c85
Hamid Reza Oujand <hmdrzaa11@gmail.com>
2021-04-04 04:25:42 +0430

    add deleteProduct by id for admin route

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -4,13 +4,22 @@ let usersController = require("../controllers/userController");
 let restrictTo = require("../utils/restrictTo");
 
 router
-    .route("/")
-    .post(
-        usersController.isAuthenticated,
-        productController.upload.single("image"),
-        productController.resizeImages,
-        restrictTo("admin"),
-        productController.createProduct
-    ).get(productController.getAllProducts)
+  .route("/")
+  .post(
+    usersController.isAuthenticated,
+    productController.upload.single("image"),
+    productController.resizeImages,
+    restrictTo("admin"),
+    productController.createProduct
+  )
+  .get(productController.getAllProducts);
+
+router
+  .route("/:productId")
+  .delete(
+    usersController.isAuthenticated,
+    restrictTo("admin"),
+    productController.deleteProduct
+  );
 
 module.exports = router;
