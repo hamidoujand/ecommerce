8cae54b241aa9b9878a97d49b1f7299f4250e52f
Hamid Reza Oujand <hmdrzaa11@gmail.com>
2021-04-03 21:46:14 +0430

    add change password route

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -3,5 +3,10 @@ let userController = require("../controllers/userController");
 
 router.post("/signup", userController.signup);
 router.post("/signin", userController.signin);
+router.patch(
+  "/change-password",
+  userController.isAuthenticated,
+  userController.changePassword
+);
 
 module.exports = router;
