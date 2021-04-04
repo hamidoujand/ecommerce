5a067574e79682c6f278256df58cbcf8d390adc1
Hamid Reza Oujand <hmdrzaa11@gmail.com>
2021-04-05 01:20:46 +0430

    add error handler for duplicated field in mongodb

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -11,12 +11,19 @@ let handleInvalidMongoId = (e) => {
   return new AppError("Invalid Id", 404);
 };
 
+let handleDuplicatedField = (e) => {
+  let duplicatedValue = e.message.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  return new AppError(`value "${duplicatedValue}" already exists`, 400);
+};
 module.exports = (err, req, res, next) => {
   if (err.name === "ValidationError") {
     err = handleMongooseValidation(err);
   }
   if (err.name === "CastError") {
     err = handleInvalidMongoId(err);
+  }
+  if (err.code === 11000) {
+    err = handleDuplicatedField(err);
   }
   let statusCode = err.statusCode || 500;
   let status = err.status || "error";
